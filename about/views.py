4a0c79cd9e3219778781fb4d342fddd9fbc8132b
DominiqueFre <dominiquereade@gmail.com>
2026-02-18 13:54:00 +0000

    added about

--- about/views.py
+++ about/views.py
@@ -6,8 +6,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    if request.method == 'POST':
-        return HttpResponse("You must have POSTed something!")
-    else:
-        return HttpResponse(f"Request method: {request.method}")
+def about_me(request):
+    return HttpResponse("This would be the about page.")
