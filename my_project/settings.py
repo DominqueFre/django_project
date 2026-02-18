3949a5c69189aec604a853da0e4f7c47a6102e15
DominiqueFre <dominiquereade@gmail.com>
2026-02-18 17:28:09 +0000

    Add heroku  tweaks

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,10 +25,12 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
+    'localhost',
+    '127.0.0.1',
 ]
 
 
